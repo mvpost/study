addf414c0e669ddd746647c0196211f3cfc449ed
Максим Постников <mvpost@bk.ru>
2012-11-21 15:32:14 +0400

    ищу косяк

---
+++ www/urlpost.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['url']))
+{
+    echo file_get_contents("http://" . SanitizeString($_POST['url']));
+} else
+{
+    echo $_POST['url'];
+}
+
+function SanitizeString($var)
+{
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    return stripslashes($var);
+}
